830a8b4652c7c232495078d6f4c3f87324d22f7d
Jason.W <jarodwang1981@gmail.com>
2022-01-04 16:49:29 +0800

    Optimized scripts
    Simplified command format, remove '' in the task list for numbers;
    Correct typos

--- serialMaster/demos/cuteNybble.py
+++ serialMaster/demos/cuteNybble.py
@@ -10,13 +10,6 @@ import numpy as np
 if __name__ == '__main__':
     try:
         flushSeialOutput(300)
-        if platform.uname()[1] != 'raspberrypi':
-            print("If there is no response in the terminal")
-            print("you should close the terminal first")
-            print("then change the value of 'bluetoothPortIndex' in the ardSerial.py (line:129)")
-            print("to connect to another blue tooth serial port")
-            print("then reopen the terminal and rerun the script")
-            print("---Start---")
 
         serialWriteByte(["ksit"])
         time.sleep(2)
