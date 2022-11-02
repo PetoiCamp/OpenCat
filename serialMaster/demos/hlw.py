828155131dc04d2f45461cd38b275e642d98f880
Rongzhong Li <rzlib2l@gmail.com>
2022-11-02 15:17:31 +0800

    Update hlw.py

--- serialMaster/demos/hlw.py
+++ serialMaster/demos/hlw.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
-
 import sys
 sys.path.append("..")
 from ardSerial import *
 
-hlw = [
+hlw = [ # customized gait that takes smaller steps ajusted for the costume
 14, 0, 0, 1,
   10,  29,  51,  46,  33,  30,   4,  15,
   10,  32,  43,  48,  40,  29,   3,  16,
@@ -21,15 +20,13 @@ hlw = [
   35,  23,  59,  40,  16,  31,  16,  14,
   26,  26,  59,  42,  19,  30,   8,  14,
   15,  28,  52,  45,  27,  30,   5,  15]
-  
-jump = [
-  -4,   0,   0,   1,
+jump = [ # customized behavior designed with the Petoi Skill Composer
+-4,   0,   0,   1,
    1,   2,   2,
    0,   0,   0,   0,   0,   0,   0,   0,  30,  30,  30,  30,  30,  30,  30,  30,   8,   0,   0,   0,
    0,   0,   0,   0,   0,   0,   0,   0,  42,  42,  42,  42,   6,   6,   6,   6,  48,   0,   0,   0,
   37,   0,   0,   0,   0,   0,   0,   0, -10, -10, -10, -10, 110, 110, 110, 110,   0,   0,   0,   0,
-  26,  11, -11,   0,  11,  11, -22, -22,   6,   6,   8,   8,   8,   8,  52,  52,   0,   0,   0,   0,
-]
+  26,  11, -11,   0,  11,  11, -22, -22,   6,   6,   8,   8,   8,   8,  52,  52,   0,   0,   0,   0,]
 
 model = 'Bittle'
 postureTable = postureDict[model]
@@ -37,26 +34,21 @@ postureTable = postureDict[model]
 E_RGB_ALL = 0
 E_RGB_RIGHT = 1
 E_RGB_LEFT = 2
-
 E_EFFECT_BREATHING = 0
 E_EFFECT_ROTATE = 1
 E_EFFECT_FLASH = 2
 E_EFFECT_NONE = 3
 
-
 if __name__ == '__main__':
     try:
-        '''
-        testSchedule is used to test various serial port commands
-        '''
-        testSchedule = [
+        testSchedule = [# the storyline
             ['g',0],                    # turn off the gyroscope
             ['C',[  0,50,  0, E_RGB_ALL, E_EFFECT_BREATHING],0],# turn on green eyes
-            ['ksit', 1],                # sit down
-            ['kck',1],                  # check around
+            ['ksit', 1],                # sit down for 1 second
+            ['kck',1],                  # check around then stop for 1 second
             ['C',[  0,  0,127, E_RGB_LEFT,  E_EFFECT_ROTATE],   0], # turn on blue eyes and rotate
-            ['kbalance', 1],            # stand up
-            ['i',[0,-10,8,30,12,30],0], # move three joints simutanuously
+            ['kbalance', 1],            # stand up then stop for 1 second
+            ['i',[0,-10,8,30,12,30],0], # move the head and two leg joints simutanuously to look down
             ['C',[127,  0,  0, E_RGB_ALL,   E_EFFECT_FLASH],  0],   # blink red eyes
             ['C',[127,  0,  0, E_RGB_ALL,   E_EFFECT_FLASH],  1],   # blink red eyes
             ['K',jump,0],               # jump
@@ -65,24 +57,22 @@ if __name__ == '__main__':
             ['I',[14,0,15,0.5]],        # lean backward
             ['C',[127,  0,  0, E_RGB_ALL,   E_EFFECT_FLASH],  0], # blink red eyes
             ['C',[127,  0,  0, E_RGB_ALL,   E_EFFECT_FLASH],  0], # blink red eyes
-            ['C',[127,  112, 0, E_RGB_ALL,  E_EFFECT_FLASH],  0], # blink yellow eyes
+            ['C',[127,112, 0, E_RGB_ALL,  E_EFFECT_FLASH],  0], # blink yellow eyes
             ['kbk', 0.5], # make the robot walk backward for 0.5 seconds
             ['kbf', 0],   # make the robot backflip
-            ['ktrF', 2],  # trot forward, run upside down
-            ['C',[127,  0,  0, E_RGB_ALL,  E_EFFECT_NONE],  0] # change eyes to red for the next round of shooting
+            ['ktrF', 2],  # trot forward, run upside down for 2 seconds
+            ['C',[127,  0,  0, E_RGB_ALL,  E_EFFECT_NONE],  0] # change eyes to red for the next round
         ]
-        
         goodPorts = {}
         connectPort(goodPorts) # discover petoi robots connected to the computer's serial ports
         t=threading.Thread(target = keepCheckingPort, args = (goodPorts,))
         t.start()
-        parallel = False
+        parallel = False # we are not controlling multiple robots in this demo
 #        if len(goodPorts)>0:
         time.sleep(2);
         for task in testSchedule:  # execute the tasks in the testSchedule
             print(task)
             send(goodPorts, task)
-        
         closeAllSerial(goodPorts)
         logger.info("finish!")
         os._exit(0)
@@ -92,3 +82,4 @@ if __name__ == '__main__':
         closeAllSerial(goodPorts)
         os._exit(0)
         raise e
+
