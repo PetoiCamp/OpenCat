37f78757d20eaaa457e8c2da52c498c4eb9c9aef
Rongzhong Li <rzlib2l@gmail.com>
2023-01-12 00:37:53 +0800

    add voice command module

--- ModuleTests/testVoiceCommander/testVoiceCommander.ino
+++ ModuleTests/testVoiceCommander/testVoiceCommander.ino
@@ -1,10 +1,19 @@
-//use the software serial port on the NyBoard to read the module. connect the module to the grove socket with pin 6 and 7.
-//if you wire the module up with the USB programmer directly, connect the module's Tx to the programmer's Rx, and Rx to Tx.
+// Petoi Voice Command Module
+// use the software serial port on the NyBoard to read the module. connect the module to the grove socket with pin 6 and 7.
+// or use the serial 2 port on the BiBoard to read the module. connect the module to the pin Tx2 and Rx2.
+// if you wire the module up with the USB programmer directly, connect the module's Tx to the programmer's Rx, and Rx to Tx.
+// Rongzhong Li
+// Petoi LLC
+// Jan 12, 2023
 
+// #define SOFTWARE_SERIAL //disable this line to use the serial 2 of BiBoard
+#ifdef SOFTWARE_SERIAL
 #include <SoftwareSerial.h>
+SoftwareSerial Serial2(6, 7);  //(Rx,Tx)
+#endif
+
 #define BAUD_RATE 115200
 #define SOFT_BAUD_RATE 9600
-SoftwareSerial mySerial(6, 7);  //(Rx,Tx)
 
 String customizedCmdList[] = {
   "first", "second", "third", "4th", "5th", "6th", "7th", "8th", "9th", "10th", "\0"  //define up to ten customized commands.
@@ -13,10 +22,10 @@ int listLength = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  mySerial.begin(SOFT_BAUD_RATE);
-  mySerial.setTimeout(5);
   Serial.begin(BAUD_RATE);
   Serial.setTimeout(5);
+  Serial2.begin(SOFT_BAUD_RATE);
+  Serial2.setTimeout(5);
   while (customizedCmdList[listLength] != "\0")
     listLength++;
   Serial.print("Number of customized commands on the main board: ");
@@ -25,14 +34,15 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (Serial.available()) {
-    String cmd = Serial.readStringUntil('\n');
-    int idx = 0;
-    Serial.println(cmd);
-    mySerial.print(cmd);
-  }
-  if (mySerial.available()) {
-    String cmd = mySerial.readStringUntil('\n');
+  // if (Serial.available()) {// send some control command directly to the module
+  //   String cmd = Serial.readStringUntil('\n');
+  //   int idx = 0;
+  //   Serial.println(cmd);
+  //   Serial2.print(cmd);
+  // }
+
+  if (Serial2.available()) {
+    String cmd = Serial2.readStringUntil('\n');
     if ((byte)cmd[2] <= 20) {
       Serial.print("Customized Command Index: ");
       int index = (byte)cmd[2] - 11;  //Convert the third byte to integer index of the customized command, starting from 0
