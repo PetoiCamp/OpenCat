22443af527c051dc7402da5a7edd792d10fa5374
Rongzhong Li <rzlib2l@gmail.com>
2023-03-06 00:12:32 +0800

    add traditional Chinese to desktop app

--- src/OpenCat.h
+++ src/OpenCat.h
@@ -1,4 +1,4 @@
-#define SOFTWARE_VERSION "N230301"  //NyBoard + YYMMDD
+#define SOFTWARE_VERSION "N230305"  //NyBoard + YYMMDD
 //board configuration
 // -- comment out these blocks to save program space for your own codes --
 
