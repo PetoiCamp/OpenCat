c87f42ace6a4eedcd783f926737c78998efb7e23
Rongzhong Li <rzlib2l@gmail.com>
2023-05-24 12:14:23 +0800

    enter calibration by tilted booting

--- src/imu.h
+++ src/imu.h
@@ -166,11 +166,11 @@ int16_t imuOffset[9] = { 0, 0, 0,
 
 // #define READ_ACCELERATION
 void print6Axis() {
-  // PT_FMT(ypr[0], 5);
-  // PT('\t');
-  // PT_FMT(ypr[1], 5);
-  // PT('\t');
-  // PT_FMT(ypr[2], 5);
+  PT_FMT(ypr[0], 5);
+  PT('\t');
+  PT_FMT(ypr[1], 5);
+  PT('\t');
+  PT_FMT(ypr[2], 5);
 #ifdef READ_ACCELERATION
   PT("\t");
   // PT(aaWorld.x);
