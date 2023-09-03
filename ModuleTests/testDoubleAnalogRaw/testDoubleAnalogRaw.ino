335e0fe529f1acb405f879c5b6012398c835d953
Rongzhong Li <rzlib2l@gmail.com>
2023-09-04 01:21:02 +0800

    Create testDoubleAnalogRaw.ino

---
+++ ModuleTests/testDoubleAnalogRaw/testDoubleAnalogRaw.ino
@@ -0,0 +1,19 @@
+#define IN1 A2
+#define IN2 A3
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(100);
+  Serial.println("start");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //stats();
+
+  Serial.print("0 1024 ");
+  Serial.print(analogRead(IN1));
+  Serial.print('\t');
+  Serial.println(analogRead(IN2));
+}
