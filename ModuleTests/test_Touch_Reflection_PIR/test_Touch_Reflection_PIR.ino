51aae6b197cb2809d1d57c4e798f692b62456d76
Jason.W <jarodwang1981@gmail.com>
2024-07-15 18:21:00 +0800

    Update test_Touch_Reflection_PIR.ino

--- ModuleTests/test_Touch_Reflection_PIR/test_Touch_Reflection_PIR.ino
+++ ModuleTests/test_Touch_Reflection_PIR/test_Touch_Reflection_PIR.ino
@@ -1,14 +1,19 @@
-//connects to the digital Grove socket with D6 and D7
+//connects to the digital Grove socket with D8 and D9
+#define DIGITAL1 8
+#define DIGITAL2 9
 int val;
 void setup()
 {
     Serial.begin(115200);
+  Serial.setTimeout(2);
+  pinMode(DIGITAL1, INPUT);
+  pinMode(DIGITAL2, INPUT);
 }
  
 void loop()
 {
-    val = digitalRead(6);
-    Serial.println(val); //prints 1 if there's human or touch event
+  val = digitalRead(DIGITAL1);
+  Serial.println(val); //prints 1 if there's human or touch event
                          //prints 0 if there's reflection within 4-16mm.
     delay(100);
 }
